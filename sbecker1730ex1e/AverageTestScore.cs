16eb8d09d779997803c113815d11e95fe1ebbcc6
Sumner Becker <xs4355qt@go.minnstate.edu>
2019-09-11 12:35:20 -0500

    Completedex1e

---
+++ sbecker1730ex1e/AverageTestScore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sbecker1730ex1e
+{
+    public partial class AverageTestScore : Form
+    {
+        public AverageTestScore()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            txtAverage.Text = (Convert.ToDecimal(txtTest1.Text) / 3m
+                + Convert.ToDecimal(txtTest2.Text) / 3m
+                + Convert.ToDecimal(txtTest3.Text) / 3m).ToString("0.00");
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            txtTest1.Text = "0";
+            txtTest2.Text = "0";
+            txtTest3.Text = "0";
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
